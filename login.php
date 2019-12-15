16fd50b0cc87b2ffe17ffcbbf0e025712cf2ad02
Thad House <thadhouse1@gmail.com>
2019-12-14 21:19:39 -0800

    Finished login.php, and base setup for signup.php

--- login.php
+++ login.php
@@ -1,17 +1,65 @@
-<?php 
-/* Main page with two forms: sign up and log in */
-require 'db.php';
-session_start();
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = "utf-8">
+    <title>Login Result</title>
+    <style type = "text/css">
+      body { font-family: sans-serif;
+             background-color: lightyellow; }
+      table { background-color: lightblue;
+      border-collapse: collapse;
+      border: 1px solid gray; }
+      td { padding: 5px; }
+      tr:nth-child(odd) {
+      background-color: white; }
+    </style>
+  </head>
+  <body>
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+    <?php
 
-    if($_POST['password'] == $_POST['passwordConf']){
+    $db_handle_name = mysqli_connect( "localhost", "webphp", "password", "finaldb");
 
-      if(isset($_POST['register']))
-      {
-          require 'register.php';
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+      $query = "SELECT * FROM auth WHERE username='" . $_POST['username'] . "'";
+      $resource_name = mysqli_query($db_handle_name, $query );
+
+      if (mysqli_num_rows($resource_name) > 0) {
+        $row = mysqli_fetch_assoc($resource_name);
+        if ($row['password'] == $_POST['password']) {
+          print("Welcome back " . $_POST['username'] . "! Login was successful<p>");
+          print ("<table rules=\"all\">");
+          print ("<tr>");
+          print ("<th>First Name</th>");
+          print ("<th>Last Name</th>");
+          print ("<th>Email</th>");
+          print ("<th>Phone Number</th>");
+          print ("</tr>");
+
+          $firstName = $row['firstName'];
+          $lastName = $row['lastName'];
+          $email = $row['email'];
+          $phone = $row['phone'];
+
+          print ("<tr>");
+          print ("<td>$firstName</td>");
+          print ("<td>$lastName</td>");
+          print ("<td>$email</td>");
+          print ("<td>$phone</td>");
+
+          print ("</tr>");
+          print ("</table>");
+
+
+
+        } else {
+          print("Incorrect username or password.");
+        }
+      } else {
+        print("Incorrect username or password.");
       }
     }
-}
-?>
+    ?>
+  </body>
+</html>
