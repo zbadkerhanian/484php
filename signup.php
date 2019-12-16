6015f724cec057aa136b386ef1ecc3234267050c
Thad House <thadhouse1@gmail.com>
2019-12-15 18:30:07 -0800

    Finish signup functionality

--- signup.php
+++ signup.php
@@ -18,10 +18,49 @@
 
     <?php
 
+    function showTable($db) {
+      $showQuery = "SELECT firstName, lastName FROM auth";
+      $resource_name = mysqli_query($db, $showQuery );
+      print ("<table rules=\"all\">");
+      print ("<tr>");
+      print ("<th>First Name</th>");
+      print ("<th>Last Name</th>");
+      print ("</tr>");
+      while ($row = mysqli_fetch_assoc($resource_name)){
+
+
+        $firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+
+        print ("<tr>");
+        print ("<td>$firstName</td>");
+        print ("<td>$lastName</td>");
+        print ("</tr>");
+      }
+
+      print ("</table>");
+    }
+
     $db_handle_name = mysqli_connect( "localhost", "webphp", "password", "finaldb");
 
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
+      $query = "INSERT INTO auth (username,password,firstName,lastName,email,phone) VALUES ('"
+          . $_POST['username'] . "', '"
+          . $_POST['password'] . "', '"
+          . $_POST['firstname'] . "', '"
+          . $_POST['lastname'] . "', '"
+          . $_POST['email'] . "', '"
+          . $_POST['phone'] . "')";
+
+      $result = mysqli_query($db_handle_name, $query );
+
+      if ($result == FALSE) {
+        print("Error creating user");
+        return;
+      }
+      print("you are registered now<p>");
+      showTable($db_handle_name);
     }
     ?>
   </body>
